c537bb38c159516a4531c94703baccae21ca8283
Luiz Eduardo <calixtoluizeduardo@gmail.com>
2025-11-21 11:57:36 -0300

    Adding CSS modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 // PascalCase
 // App
 // ExemploDeComponente
+import "./styles/theme.css";
+import "./styles/global.css";
+import { Heading } from "./components/Heading";
 
 export function App() {
   return (
-    <div>
-      <h1>olá mundo (do App)</h1>
+    <>
+      <Heading />
       <p>
         Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis,
         earum! Sequi quam earum quibusdam harum fuga nesciunt? Veniam enim porro
         eligendi voluptatum ipsa, velit totam quae excepturi quidem magni optio?
       </p>
-    </div>
+    </>
   );
 }
 
