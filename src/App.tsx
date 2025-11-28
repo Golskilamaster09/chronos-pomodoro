fb7463d6b68c6fe7c22bab7894c01aca24845a20
Luiz Eduardo <calixtoluizeduardo@gmail.com>
2025-11-28 10:38:30 -0300

    Adding menu link and logo components

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,10 @@
 
 import "./styles/theme.css";
 import "./styles/global.css";
+
 import { Container } from "./components/container";
-import { Heading } from "./components/Heading";
 import { Logo } from "./components/Logo";
+import { Menu } from "./components/Menu";
 
 export function App() {
   return (
@@ -15,7 +16,7 @@ export function App() {
         <Logo />
       </Container>
       <Container>
-        <Heading>MENU</Heading>
+        <Menu />
       </Container>
     </>
   );
