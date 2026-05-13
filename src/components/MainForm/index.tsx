3662d11765a9d318ee02273d652d1cfb4c93b93d
Luiz <le.calixto@hotmail.com>
2026-05-13 11:06:39 -0300

    Feat(DefaultInput): The default form submission has been interrupted

--- src/components/MainForm/index.tsx
+++ src/components/MainForm/index.tsx
@@ -2,17 +2,26 @@ import { PlayCircleIcon } from "lucide-react";
 import { Cycles } from "../Cycles";
 import { Defaultbutton } from "../DefaultButton";
 import { Defaultinput } from "../Defaultinput";
-import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
+import { useRef } from "react";
 
 export function MainForm() {
+  const taskNameInput = useRef<HTMLInputElement>(null);
+
+  function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    console.log("deu certo");
+  }
+
   return (
-    <form action="" className="form">
+    <form onSubmit={handleCreateNewTask} action="" className="form">
       <div className="formRow">
         <Defaultinput
           labelText="task"
           type="text"
           id="meuInput"
           placeholder="Digite algo"
+          ref={taskNameInput}
         />
       </div>
 
